1640b3287ecfc24bffa7c47f2b74828e65b80475
Takenori Oshima <takenorioshima@gmail.com>
2024-05-24 14:56:07 +0900

    update: Use ResponsiveAnalogRead

--- midi-merger-cv-to-midi.ino
+++ midi-merger-cv-to-midi.ino
@@ -1,9 +1,13 @@
 #include <MIDI.h>
+#include <ResponsiveAnalogRead.h>
 
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 const int gateInPin = A0;
 const int cvInPin = A1;
+
+ResponsiveAnalogRead analogGateIn(gateInPin, true);
+ResponsiveAnalogRead analogCvIn(cvInPin, true);
 
 int gateInValue = 0;
 int cvInValue = 0;
@@ -15,17 +19,25 @@ void setup() {
 }
 
 void loop() {
-  gateInValue = map(analogRead(gateInPin), 0, 1023, 0, 127);
-  cvInValue = map(analogRead(cvInPin), 0, 1023, 0, 127);
-  
-  if( gateInValue != gateInPrevValue ){
-    MIDI.sendControlChange(45, gateInValue, 1);
-    gateInPrevValue = gateInValue;
+  analogGateIn.update();
+  analogCvIn.update();
+
+  // Gate input.
+  if(analogGateIn.hasChanged()) {
+    gateInValue = (analogGateIn.getValue() < 512) ? 0 :127;
+    if( gateInValue != gateInPrevValue ){
+      MIDI.sendControlChange(46, gateInValue, 1);
+      gateInPrevValue = gateInValue;
+    }
   }
 
-  if( cvInValue != cvInPrevValue ){
-    MIDI.sendControlChange(46, cvInValue, 1);
-    cvInPrevValue = cvInValue;
+  // CV input.
+  if(analogCvIn.hasChanged()) {
+    cvInValue = map(analogCvIn.getValue(), 0, 1023, 0, 127);
+    if( cvInValue != cvInPrevValue ){
+      MIDI.sendControlChange(47, cvInValue, 1);
+      cvInPrevValue = cvInValue;
+    }
   }
 
   MIDI.read();

302c316ad24976e38e2addd3a6edb67ea7d39949
Takenori Oshima <takenorioshima@gmail.com>
2024-05-31 13:40:52 +0900

    update: Add status LEDs

--- midi-merger-cv-to-midi.ino
+++ midi-merger-cv-to-midi.ino
@@ -14,31 +14,52 @@ int cvInValue = 0;
 int gateInPrevValue;
 int cvInPrevValue;
 
+const long ledInterval = 50;  // millsecconds.
+unsigned long prevMillisGate = 0;
+unsigned long prevMillisCv = 0;
+
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  pinMode(5, OUTPUT);  // LED_GATE - PD5
+  pinMode(6, OUTPUT);  // LED_CV - PD6
 }
 
 void loop() {
   analogGateIn.update();
   analogCvIn.update();
 
+  unsigned long currentMillis = millis();
+
   // Gate input.
-  if(analogGateIn.hasChanged()) {
-    gateInValue = (analogGateIn.getValue() < 512) ? 0 :127;
-    if( gateInValue != gateInPrevValue ){
-      MIDI.sendControlChange(46, gateInValue, 1);
+  if (analogGateIn.hasChanged()) {
+    gateInValue = (analogGateIn.getValue() < 512) ? 0 : 127;
+    if (gateInValue != gateInPrevValue) {
+      MIDI.sendControlChange(79, gateInValue, 1);
+      if (gateInValue == 127) {
+        digitalWrite(5, HIGH);
+        prevMillisGate = currentMillis;
+      }
       gateInPrevValue = gateInValue;
     }
   }
 
   // CV input.
-  if(analogCvIn.hasChanged()) {
-    cvInValue = map(analogCvIn.getValue(), 0, 1023, 0, 127);
-    if( cvInValue != cvInPrevValue ){
+  if (analogCvIn.hasChanged()) {
+    cvInValue = map(analogCvIn.getValue(), 255, 768, 0, 127);
+    if (cvInValue != cvInPrevValue) {
       MIDI.sendControlChange(47, cvInValue, 1);
+      digitalWrite(6, HIGH);
+      prevMillisCv = currentMillis;
       cvInPrevValue = cvInValue;
     }
   }
 
-  MIDI.read();
+  // Turn off LEDs.
+  if (currentMillis - prevMillisGate >= ledInterval) {
+    digitalWrite(5, LOW);
+  }
+  if (currentMillis - prevMillisCv >= ledInterval) {
+    digitalWrite(6, LOW);
+  }
 }
